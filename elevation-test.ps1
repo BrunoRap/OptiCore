6458f5ab1b3a7a332aaf3f4590c3655ee3e5ddd1
Bruno Raposo <brunorap@gmail.com>
2026-06-07 08:04:08 -0300

    chore: clean repo - keep only README

--- elevation-test.ps1
+++
@@ -1,5 +0,0 @@
-﻿$id = [Security.Principal.WindowsIdentity]::GetCurrent()
-$p  = [Security.Principal.WindowsPrincipal]$id
-$isAdmin = $p.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-"IsAdmin=$isAdmin" | Set-Content "C:\OptiCore\elevation-test.txt" -Encoding UTF8
-"User=$($id.Name)" | Add-Content "C:\OptiCore\elevation-test.txt" -Encoding UTF8
